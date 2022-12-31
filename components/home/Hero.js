e87561906052fbc0050c712651b0628f135ad656
Gabe <gabrielloubier1@gmail.com>
2022-12-31 13:13:40 -0500

    homepage images

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -84,12 +84,11 @@ export default function Hero() {
                         <div className="relative h-64 overflow-hidden rounded-lg w-44 sm:opacity-0 lg:opacity-100 bg-teal-300/25">
                           <Image
                             fill
-                            sizes="33vw"
+                            sizes="(max-width: 768px) 50vw, 33vw"
+                            className={`object-cover`}
                             alt="Cow Store Steak"
                             priority={true}
-                            as="image"
-                            src="/assets/steak/1.jpg"
-                            className="object-cover object-center w-full h-full "
+                            src="/assets/steak/4.jpg"
                           />
                         </div>
                         <div className="relative h-64 overflow-hidden rounded-lg w-44 bg-teal-300/40">
@@ -99,10 +98,12 @@ export default function Hero() {
                             // width={264}
                             // height={256}
                             fill
-                            sizes="33vw"
+                            sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+                            className={`object-cover`}
                             alt="Cow Store Steak"
                             src="/assets/steak/2.jpg"
-                            className="object-cover object-center w-full h-full "
                           />
                         </div>
                       </div>
@@ -115,12 +116,14 @@ export default function Hero() {
                             // width={264}
                             // height={256}
                             fill
+                            sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              20vw"
+                            className={`object-cover`}
                             priority={true}
                             as="image"
-                            sizes="33vw"
                             alt="Cow Store Steak"
                             src="/assets/steak/3.jpg"
-                            className="object-cover object-center w-full h-full "
                           />
                         </div>
 
@@ -131,10 +134,13 @@ export default function Hero() {
                             // width={264}
                             // height={256}
                             fill
-                            sizes="33vw"
+                            sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+                            className={`object-cover`}
                             alt="Cow Store Steak"
                             src="/assets/steak/4.jpg"
-                            className="object-cover object-center w-full h-full"
+                            // className="object-cover object-center w-full h-full"
                           />
                         </div>
                         <div className="relative h-64 overflow-hidden rounded-lg w-44 bg-teal-300/25">
@@ -144,7 +150,10 @@ export default function Hero() {
                             // width={264}
                             // height={256}
                             fill
-                            sizes="33vw"
+                            sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+                            className={`object-cover`}
                             alt="Cow Store Steak"
                             src="/assets/steak/5.jpg"
                             className="object-cover object-center w-full h-full"
@@ -158,7 +167,10 @@ export default function Hero() {
                             // blurDataURL={rgbDataURL(153, 246, 228)}
                             // width={256}
                             fill
-                            sizes="33vw"
+                            sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+                            className={`object-cover`}
                             alt="Cow Store Steak"
                             // height={256}
                             src="/assets/steak/6.jpg"
@@ -172,7 +184,10 @@ export default function Hero() {
                             // width={264}
                             // height={256}
                             fill
-                            sizes="33vw"
+                            sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+                            className={`object-cover`}
                             alt="Cow Store Steak"
                             src="/assets/steak/12.jpg"
                             className="object-cover object-center w-full h-full"
